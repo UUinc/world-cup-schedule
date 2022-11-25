5a54183cb5f52127520055fe550e26c31bdbf9ba
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-11-25 22:26:35 +0100

    update AppHeader

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -7,7 +7,7 @@ function AppHeader() {
         <div className="Header">
             <div className="Header-Top">
                 <img src={logo} alt="world cup qatar 2022 logo" width="80px" />
-                <h1>Fifa World Cup Qatar 2022 - Schedule</h1>;
+                <h1>Fifa World Cup Qatar 2022 - Schedule</h1>
             </div>
         </div>
     );
